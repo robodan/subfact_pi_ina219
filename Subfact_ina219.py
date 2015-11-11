87fca69bdafea25d74451c7c7e84b2fded948722
Dan Christian <DanChristian65@gmail.com>
2015-11-11 12:05:30 -0700

    Use official Adafruid_I2C not included version
    Remove old Adafruid_I2C
    Import from Adafruid_I2C as a module
    getBusVoltage needs big endian readU16

--- Subfact_ina219.py
+++ Subfact_ina219.py
@@ -96,7 +96,7 @@ class INA219:
 
 	# Constructor
 	def __init__(self, address=0x40, debug=False):
-		self.i2c = Adafruit_I2C(address, debug=False)
+		self.i2c = Adafruit_I2C.Adafruit_I2C(address, debug=debug)
 		self.address = address
 		self.debug = debug
 		
@@ -129,7 +129,7 @@ class INA219:
 		self.i2c.writeList(self.__INA219_REG_CONFIG, bytes)
 
 	def getBusVoltage_raw(self):
-		result = self.i2c.readU16(self.__INA219_REG_BUSVOLTAGE)
+		result = self.i2c.readU16(self.__INA219_REG_BUSVOLTAGE, little_endian=False)
 		
 		# Shift to the right 3 to drop CNVR and OVF and multiply by LSB
 		return (result >> 3) * 4
